828ac1bd75a89dc64a17ac7ee931fbd73cd2465e
diego69-foda <diego.bkuhn@gmail.com>
2025-10-08 14:36:24 -0300

    correções de erros

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -11,17 +11,17 @@ export class Triangle implements Figure {
   b: number;
   c: number;
 
-  constructor(color, a: number, b: number, c: number) {
+  constructor(color: 'red' | 'blue' | 'green', a: number, b: number, c: number) {
     this.color = color;
     this.a = a;
     this.b = b;
     this.c = c;
     if (a <= 0 || b <= 0 || c <= 0) {
-      throw new Error('Sides must be positive numbers');
+      throw new Error(`Sides must be positive numbers: a= ${a}, b= ${b}, c= ${c}`);
     }
 
     if (a + b <= c || a + c <= b || b + c <= a) {
-      throw new Error('Triangle inequality violated');
+      throw new Error(`Sides ${a}, ${b} and ${c} can't form a triangle`);
     }
   }
 
@@ -36,13 +36,14 @@ export class Circle implements Figure {
   color: 'red' | 'blue' | 'green';
   shape: 'circle' = 'circle';
 
-  constructor(color, radius: number) {
+  constructor(color: 'red' | 'blue' | 'green', radius: number) {
     this.radius = radius;
     this.color = color;
     if (radius <= 0) {
-      throw new Error('Radius must be a positive number');
+      throw new Error(`Radius must be > 0 (got ${radius})`);
     }
   }
+
   getArea(): number {
     return Math.PI * Math.pow(this.radius, 2);
   }
@@ -54,14 +55,15 @@ export class Rectangle implements Figure {
   color: 'red' | 'blue' | 'green';
   shape: 'rectangle' = 'rectangle';
 
-  constructor(color, width: number, height: number) {
+  constructor(color: 'red' | 'blue' | 'green' , width: number, height: number) {
     this.width = width;
     this.height = height;
     this.color = color;
     if (width <= 0 || height <= 0) {
-      throw new Error('Width and height must be positive numbers');
+      throw new Error(`Width and height must be > 0 (got width=${width}, height=${height})`);
     }
   }
+  
   getArea(): number {
     return this.width * this.height;
   }
